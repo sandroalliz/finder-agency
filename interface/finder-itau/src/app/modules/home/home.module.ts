fccbe3a3a64f275f73b4b5832a2d5b6eed859089
Sandro Alliz <sandroalliz@gmail.com>
2020-05-21 15:48:50 -0300

    create header componen

--- interface/finder-itau/src/app/modules/home/home.module.ts
+++ interface/finder-itau/src/app/modules/home/home.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './page/home.component';
 import { HomeService } from './service/home.service';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HeaderComponent],
   imports: [
     CommonModule
   ],
